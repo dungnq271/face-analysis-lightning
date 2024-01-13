c1ce0aa33f49459fb107ee2d574ea221fdc73124
dungnq271 <dungnguyen2712002@gmail.com>
2024-01-13 23:34:24 +0700

    Visualize both original and v2 face images

--- notebooks/exploratory/0.1-visualize-some-classes.py
+++ notebooks/exploratory/0.1-visualize-some-classes.py
@@ -9,6 +9,7 @@ from src.reproducible_code.tools import plot
 # %%
 data_dir = "../../data/interim"
 img_dir = osp.join(data_dir, "cropped_faces")
+org_img_dir = osp.join(data_dir, "cropped_faces_groundtruth")
 label_file = osp.join(data_dir, "labels.csv")
 
 # %%
@@ -26,7 +27,10 @@ races
 for race in races:
     print(race)
     df = labels_df[labels_df["race"] == race].sample(16)
-    fps = [osp.join(img_dir, fn) for fn in df.face_file_name.tolist()]
+    fns = df.face_file_name.tolist()
+    fps = [osp.join(img_dir, fn) for fn in fns]
+    org_fps = [osp.join(org_img_dir, fn) for fn in fns]    
     plot.display_multiple_images(fps)
+    plot.display_multiple_images(org_fps)    
 
 # %%
